838c02f4aeff24c454b00eb16bb2848b7c39ace9
Silas Matos <silas.progamacao@gmail.com>
2024-11-21 13:20:09 -0300

    att

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Rotas from './routes/router'
+import Rotas from './routes/Router'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
